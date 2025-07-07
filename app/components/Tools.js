71f1470e04f14fb7cb56134a533803406d6ebab8
Pratham-008 <patelprathamkanubhai@gmail.com>
2025-07-07 20:25:10 +0530

    final commit

--- app/components/Tools.js
+++ app/components/Tools.js
@@ -1,98 +1,137 @@
 export default function Tools() {
   return (
     <div className="flex flex-col gap-5 py-2">
-        <h1 className="font-bold text-md">Frontend</h1>
+      <h1 className="font-bold text-md">Frontend</h1>
       <div className="grid grid-cols-5 gap-5 gap-y-5 justify-items-center">
-        
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-react-original colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">React.js</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            React.js
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
-          <i className="devicon-nextjs-plain colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Next.js</div>
+          <i className="devicon-nextjs-plain colored text-5xl text-zinc-800 dark:text-white"></i>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Next.js
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-tailwindcss-original colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Tailwindcss</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Tailwindcss
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-bootstrap-plain colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Bootstrap</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Bootstrap
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-javascript-plain colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">JavaScript</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            JavaScript
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-typescript-plain colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">TypeScript</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            TypeScript
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-html5-plain-wordmark colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">HTML</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            HTML
+          </div>
         </div>
       </div>
-       <h1 className="font-bold text-md">Backend</h1>
+      <h1 className="font-bold text-md">Backend</h1>
       <div className="grid grid-cols-5 gap-5 gap-y-5 justify-items-center">
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
-          <i className="devicon-express-original-wordmark font-bold  text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Express.js</div>
+          <i className="devicon-express-original-wordmark text-5xl text-black dark:text-white"></i>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Express.js
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-nodejs-plain-wordmark colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Node.js</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Node.js
+          </div>
         </div>
 
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-mongodb-plain-wordmark colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Mongodb</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Mongodb
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-mysql-plain-wordmark colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">MySQL</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            MySQL
+          </div>
         </div>
       </div>
-       <h1 className="font-bold text-md">Programming Languages</h1>
+      <h1 className="font-bold text-md">Programming Languages</h1>
       <div className="grid grid-cols-5 gap-5 gap-y-5 justify-items-center">
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-java-plain-wordmark colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Java</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Java
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-cplusplus-plain colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">C++</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            C++
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-c-plain colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">C</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            C
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-python-plain-wordmark colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Python</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Python
+          </div>
         </div>
       </div>
-       <h1 className="font-bold text-md">Tools</h1>
+      <h1 className="font-bold text-md">Tools</h1>
       <div className="grid grid-cols-5 gap-5 gap-y-5 justify-items-center">
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
-          <i className="devicon-vercel-original-wordmark colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400">Vercel</div>
+          <i className="devicon-vercel-original-wordmark colored text-5xl text-zinc-800 dark:text-white"></i>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400">
+            Vercel
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-git-plain colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Git</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Git
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-postman-plain-wordmark colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Postman</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Postman
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
-          <i className="devicon-github-original colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Github</div>
+          <i className="devicon-github-original text-5xl text-zinc-800 dark:text-white"></i>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Github
+          </div>
         </div>
         <div className="flex-col gap-3 hover:-translate-y-2 transition-all duration-300 ease-out text-center">
           <i className="devicon-vscode-plain colored text-5xl"></i>
-          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">Vscode</div>
+          <div className="text-center text-sm text-zinc-500 dark:text-zinc-400 ">
+            Vscode
+          </div>
         </div>
       </div>
     </div>
