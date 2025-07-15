e55e88cac9d053da75af2df53f5ee235eda7eb77
Pratham-008 <patelprathamkanubhai@gmail.com>
2025-07-15 21:37:23 +0530

    final commit

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,33 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "Protfolio",
-  description: "My awesome portfolio",
-  icons: {
-    icon: "/My-favicon.png", // updated path
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
-      <body className="text-zinc-900 dark:text-zinc-100 gap-3 max-w-xl mx-auto px-4 py-4 min-h-svh flex flex-col">
+      <head>
+        <link
+          rel="stylesheet"
+          href="../public/font-awesome/css/font-awesome.min.css"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body className="text-zinc-900 dark:text-zinc-100 max-w-xl mx-auto px-4 py-4 min-h-svh flex flex-col">
         {children}
       </body>
     </html>
